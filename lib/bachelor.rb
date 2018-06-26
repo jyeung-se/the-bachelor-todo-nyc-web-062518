6b150382b60acb6944778f360ae011772f28eac5
Jack Yeung <jyeung.se@gmail.com>
2018-06-26 01:35:10 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,14 +1,18 @@
+require "pry"
+
 def get_first_name_of_season_winner(data, season)
   first_name = ""
   data.each do |season_num, info|
     if season_num == season && info == "name"
       info.each do |value|
         first_name = value.split(" ")[0]
+        binding.pry
       end
     end
   end
   first_name
 end
+
 
 def get_contestant_name(data, occupation)
   # code here
